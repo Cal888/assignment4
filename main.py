e54fafb2191db8a8c686105946195e428fc02a0b
California Moreno <calmoreno8@gmail.com>
2026-02-16 00:25:18 -0800

    added main.py REPL calculator

---
+++ main.py
@@ -0,0 +1,12 @@
+"""
+main.py
+
+Allows users to perform mathematical operations interactively with the REPL calculator.
+"""
+
+from app.calculator import calculator # Import the calculator function
+
+if __name__ == "__main__":
+    # Run the REPL calculator
+    # This block will execute only when main.py is executed.
+    calculator()
